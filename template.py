efa80fc1eb609cbb4af499e8f5318eb6fa0dc243
Timur <timurminer@gmail.com>
2019-10-31 20:47:33 +0300

    Python template update

--- template.py
+++ template.py
@@ -1,8 +1,9 @@
 # Template #
-# Author(s): <name> or <name1>, <name2>
-# Creation: <date>
+__author__ = '<name> or <name1>, <name2>'  # For code writers
+__credits__ = '<name> or <name1>, <name2>'  # For code fixers and testers
+__creation__ = '<date>'  # File creation date
 
 
 # Example #
-# Author(s): Guido van Rossum
-# Creation: 14/10/2019
+__author__ = 'Guido van Rossum'
+__creation__ = '14/10/2019'
